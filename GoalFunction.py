21b3bd3956766905052ebfa7061f270a1cab3a56
Vinícius <vfulber@inf.ufsm.br>
2018-09-24 14:14:04 -0300

    Composition method development

---
+++ GoalFunction.py
@@ -0,0 +1,144 @@
+######## GOAL FUNCTION CLASS DESCRIPTION ########
+
+#PROJECT: NFV FLERAS (FLExible Resource Allocation Service) 
+#CREATED BY: VINICIUS FULBER GARCIA
+#CONTACT: vfulber@inf.ufsm.br
+
+#RECEIVES A VALID SFC REQUEST AND CREATES THE
+#GOAL FUNCTION DESCRIBED BY IT. AFTER, IT MAKES
+#AVAILABLE A EVALUATION FUNCTION TO RECEIVE 
+#ARGUMENTS, EVALUATE THEM AND RETURNS IT RESULTS.
+
+#THE CLASS STATUS ATTRIBUTE INDICATE ITS 
+#OPERATIONS RESULTS CODES:
+
+#NORMAL CODES ->
+#0: NO TOPOLOGY EXPANDED
+#1: AVAILABLE EXPANDED TOPOLOGIES
+
+#################################################
+
+import copy
+
+from SFCRequest import SFCRequest
+
+class GoalFunction:
+	__status = None
+
+	__sfcFunction = None
+	__evaluationAggregate = None
+
+	######## CONSTRUCTOR ########
+
+	def __init__(self, sfcRequest):
+
+		if sfcRequest != None:
+			self.gfSetup(sfcRequest)
+		else:
+			self.__status = 0
+		
+	######## PRIVATE METHODS ########
+
+	def __gfEvaluateMetric(self, metric, value):
+
+		if (metric["EVALUATION"] == "MULT"):
+			return metric["INPUT"] * value
+		if (metric["EVALUATION"] == "DIV"):
+			return metric["INPUT"] / value
+		if (metric["EVALUATION"] == "ADD"):
+			return metric["INPUT"] + value
+		if (metric["EVALUATION"] == "SUB"):
+			return metric["INPUT"] - value
+
+	def __gfUpdateMetric(self, update, operation, value):
+		
+		if (operation["EVALUATION"] == "MULT"):
+			update["INPUT"] = update["INPUT"] * value
+		if (operation["EVALUATION"] == "DIV"):
+			update["INPUT"] = update["INPUT"] / value
+		if (operation["EVALUATION"] == "ADD"):
+			update["INPUT"] = update["INPUT"] + value
+		if (operation["EVALUATION"] == "SUB"):
+			update["INPUT"] = update["INPUT"] - value
+
+	######## PUBLIC METHODS ########
+
+	def gfSetup(self, sfcRequest):
+
+		self.__sfcFunction = {}
+		self.__evaluationAggregate = {}
+
+		for metric in sfcRequest.srFunction():
+			self.__sfcFunction[metric["METRIC"]] = metric.copy()
+			self.__evaluationAggregate[metric["METRIC"]] = 0
+
+		self.__status = 1
+
+	def gfBranchSetup(self, sfcFunction, branchOperations, branchValues):
+
+		self.__sfcFunction = copy.deepcopy(sfcFunction)
+		self.__evaluationAggregate = {}
+
+		for metric in self.__sfcFunction:
+			
+			self.__evaluationAggregate[metric] = 0
+			if (branchOperations[metric] == "MULT"):
+				self.__sfcFunction[metric]["INPUT"] = self.__sfcFunction[metric]["INPUT"] * branchValues[metric]
+			if (branchOperations[metric] == "DIV"):
+				self.__sfcFunction[metric]["INPUT"] = self.__sfcFunction[metric]["INPUT"] / branchValues[metric]
+			if (branchOperations[metric] == "ADD"):
+				self.__sfcFunction[metric]["INPUT"] = self.__sfcFunction[metric]["INPUT"] + branchValues[metric]
+			if (branchOperations[metric] == "SUB"):
+				self.__sfcFunction[metric]["INPUT"] = self.__sfcFunction[metric]["INPUT"] - branchValues[metric]
+
+		self.__status = 1
+
+	def gfBranchUnify(self, segmentsResults):
+
+		for metric in self.__sfcFunction:
+			self.__sfcFunction[metric]["INPUT"] = 0
+
+			for result in segmentsResults:
+				self.__evaluationAggregate[metric] += result.gfAggregation()[metric]
+				self.__sfcFunction[metric]["INPUT"] += result.gfFunction()[metric]["INPUT"]
+
+	def gfOnlyEvaluate(self, elementValues):
+
+		results = {}
+		for metric in self.__sfcFunction:
+			results[metric] = self.__gfEvaluateMetric(self.__sfcFunction[metric], elementValues[metric])
+
+		return results
+
+	def gfOnlyUpdate(self, elementValues):
+		
+		for metric in self.__sfcFunction:
+			updater = self.__sfcFunction[metric]["UPDATE"]
+			self.__gfUpdateMetric(self.__sfcFunction[metric], self.__sfcFunction[updater], elementValues[updater])
+
+	def gfProcess(self, elementValues):
+		
+		results = self.gfOnlyEvaluate(elementValues)
+		for evaluation in results:
+			self.__evaluationAggregate[evaluation] += results[evaluation]
+		self.gfOnlyUpdate(elementValues)
+
+	def gfStatus(self):
+
+		return self.__status
+
+	def gfFunction(self):
+
+		if self.__status != 1:
+			return None
+
+		return self.__sfcFunction
+
+	def gfAggregation(self):
+
+		if self.__status != 1:
+			return None
+
+		return self.__evaluationAggregate
+
+######## GOAL FUNCTION CLASS END ########
---
+++ SFCComposition.py
@@ -0,0 +1,265 @@
+######## SFC COMPOSITION CLASS DESCRIPTION ########
+
+#PROJECT: NFV FLERAS (FLExible Resource Allocation Service) 
+#CREATED BY: VINICIUS FULBER GARCIA
+#CONTACT: vfulber@inf.ufsm.br
+
+#RECEIVES A VALID SFC REQUEST AND EXECUTES A FULL
+#COMPOSITION EVALUATION (MADE WITH THE GOAL FUNCTION
+#CLASS), CREATING INDEXES FOR EVERY AVAILABLE SFC 
+#TOPOLOGY (RETRIVED FROM THE SFC TOPOLOGY CLASS).
+
+#THE CLASS STATUS ATTRIBUTE INDICATE ITS 
+#OPERATIONS RESULTS CODES:
+
+#NORMAL CODES ->
+#0: NO TOPOLOGY EXPANDED
+#1: SFC TOPOLOGIES READY TO BE ANALYZED
+#2: SFC TOPOLOGIES ALREADY ANALYZED
+
+#################################################
+
+import copy
+
+from SFCRequest import SFCRequest
+from SFCTopology import SFCTopology
+from SFCExpansion import SFCExpansion
+from GoalFunction import GoalFunction
+
+class SFCComposition:
+	__status = None
+
+	__sfcRequest = None
+	__sfcDictionary = None
+
+	__oElementsData = None
+	__branchesData = None
+
+	__aggregateDictionary = None
+	__normalizedDicitionary = None
+	__indexesDictionary = None
+
+	######## CONSTRUCTOR ########
+
+	def __init__(self, sfcRequest, sfcList):
+
+		if sfcRequest != None or sfcList == None:
+			self.scSetup(sfcRequest, sfcList)
+		else:
+			self.__status = 0
+
+	######## PRIVATE METHODS ########
+
+	def __scBranchesPrepare(self):
+
+		sfcBranches = self.__sfcRequest.srBranches()
+		branchUpdate = {}
+		branchFactors = []
+
+		if len(sfcBranches) > 0:
+
+			metricKeys = list(sfcBranches.keys())
+			for metric in metricKeys:
+				branchUpdate[metric] = sfcBranches[metric]["UPDATE"]
+
+			for branchIndex in range(len(sfcBranches[metricKeys[0]]["FACTORS"])):
+				branchStructure = []
+				for factorIndex in range(len(sfcBranches[metricKeys[0]]["FACTORS"][branchIndex])):
+					segmentDict = {}
+					for metric in metricKeys:
+						segmentDict[metric] = sfcBranches[metric]["FACTORS"][branchIndex][factorIndex]
+					branchStructure.append(segmentDict)
+				branchFactors.append(branchStructure)
+
+		self.__branchesData = (branchUpdate, branchFactors)
+
+	def __scOElementsPrepare(self):
+
+		sfcOElements = self.__sfcRequest.srFullOPEs()
+		self.__oElementsData = {}
+
+		for OE in sfcOElements:
+			self.__oElementsData[OE["ID"]] = OE
+
+	def __scEvaluateSingle(self, index):
+
+		activeInstance = GoalFunction(self.__sfcRequest)
+		funcInstances = [activeInstance]
+		funcSaves = []
+		actualBranch = 0
+		actualSegment = 0
+		nextBranch = 1
+
+		sfcElements = self.__sfcDictionary[index].split()
+		boundaryEPs = self.__sfcRequest.srEPs()
+
+		for eIndex in range(1, len(sfcElements)):
+			
+			if sfcElements[eIndex] == '{':
+				funcSaves.append((actualBranch, actualSegment))
+				actualBranch = nextBranch
+				actualSegment = 0
+				nextBranch += 1
+
+				newInstance = GoalFunction(None)
+				newInstance.gfBranchSetup(activeInstance.gfFunction(), self.__branchesData[0], self.__branchesData[1][actualBranch-1][actualSegment])
+				funcInstances.append(newInstance)
+				activeInstance = newInstance
+				continue
+
+			if sfcElements[eIndex] == '}':
+				segmentsList = []
+				for turn in range(actualSegment+1):
+					segmentsList.insert(0, funcInstances.pop())
+
+				activeInstance = funcInstances[-1]
+				activeInstance.gfBranchUnify(segmentsList)
+
+				save = funcSaves.pop()
+				actualBranch = save[0]
+				actualSegment = save[1]
+				continue
+
+			if sfcElements[eIndex] == '/':
+				actualSegment += 1
+
+				newInstance = GoalFunction(None)
+				newInstance.gfBranchSetup(funcInstances[(actualSegment+1)*-1].gfFunction(), self.__branchesData[0], self.__branchesData[1][actualBranch-1][actualSegment])
+				funcInstances.append(newInstance)
+				activeInstance = newInstance
+				continue
+
+			if not sfcElements[eIndex] in boundaryEPs:
+				activeInstance.gfProcess(self.__oElementsData[sfcElements[eIndex]])
+
+		self.__aggregateDictionary[index] = activeInstance.gfAggregation()
+
+	def __scBoundaryFactor(self):
+
+		factorList = {}
+		maximum = copy.deepcopy(self.__aggregateDictionary[0])
+		minimum = copy.deepcopy(self.__aggregateDictionary[0])
+
+		for index in range(1, len(self.__aggregateDictionary)):
+			for metric in self.__aggregateDictionary[index]:
+				if maximum[metric] < self.__aggregateDictionary[index][metric]:
+					maximum[metric] = self.__aggregateDictionary[index][metric]
+					continue
+				if minimum[metric] > self.__aggregateDictionary[index][metric]:
+					minimum[metric] = self.__aggregateDictionary[index][metric]
+
+		for metric in maximum:
+			factorList[metric] = maximum[metric] - minimum[metric]
+
+		return (factorList, maximum, minimum)
+
+	def __scNormalizeAggregations(self):
+
+		boundaryData = self.__scBoundaryFactor()
+		boundaryFactor = boundaryData[0]
+		boundaryMin = boundaryData[2]
+
+		for index in self.__normalizedDicitionary:
+			for metric in boundaryFactor:
+				if boundaryFactor[metric] != 0:
+					self.__normalizedDicitionary[index][metric] = (self.__normalizedDicitionary[index][metric] - boundaryMin[metric]) / boundaryFactor[metric]
+				else:
+					self.__normalizedDicitionary[index][metric] = 0
+
+	def __scWeightNormalizations(self):
+
+		weights = self.__sfcRequest.srWeights()
+		
+		for index in self.__normalizedDicitionary:
+			sfcIndex = 0
+			for metric in weights:
+				sfcIndex += self.__normalizedDicitionary[index][metric] * weights[metric]
+			self.__indexesDictionary[index] = sfcIndex
+
+	######## PUBLIC METHODS ########
+
+	def scSetup(self, sfcRequest, sfcList):
+
+		self.__sfcRequest = sfcRequest
+		self.__sfcDictionary = {}
+		for index in range(len(sfcList)):
+			self.__sfcDictionary[index] = sfcList[index]
+		self.__scBranchesPrepare()
+		self.__scOElementsPrepare()
+
+		self.__status = 1
+
+	def scEvaluate(self):
+
+		if self.__status != 1:
+			return
+
+		self.__aggregateDictionary = {}
+		for index in self.__sfcDictionary:
+			self.__scEvaluateSingle(index)
+
+		self.__normalizedDicitionary = copy.deepcopy(self.__aggregateDictionary)
+		self.__indexesDictionary = {}	
+
+		self.__scNormalizeAggregations()
+		self.__scWeightNormalizations()
+
+		self.__status = 2
+
+	def scBestTopology(self):
+
+		if self.__status != 2:
+			return None
+
+		goal = self.__sfcRequest.srGoal()
+		if goal == "MIN":
+			key = min(self.__indexesDictionary, key = self.__indexesDictionary.get)
+		if goal == "MAX":
+			key = max(self.__indexesDictionary, key = self.__indexesDictionary.get)
+
+		return self.__sfcDictionary[key]
+
+	def scSFCIndexes(self):
+
+		if self.__status != 2:
+			return None
+
+		resultList = []
+		for index in self.__sfcDictionary:
+			resultList.append((self.__sfcDictionary[index], self.__indexesDictionary[index]))
+
+		return resultList
+
+	def scStatus(self):
+
+		return self.__status
+
+	def scSFCKeys(self):
+
+		if self.__status != 1:
+			return None
+
+		return self.__sfcDictionary
+
+	def scAggregation(self):
+
+		if self.__status != 2:
+			return None
+
+		return self.__aggregateDictionary
+
+	def scNormalizations(self):
+		
+		if self.__status != 2:
+			return None
+
+		return self.__normalizedDicitionary
+
+	def scIndexes(self):
+
+		if self.__status != 2:
+			return None
+
+		return self.__indexesDictionary
+
+######## SFC COMPOSITION CLASS END ########
